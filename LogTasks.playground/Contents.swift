74895ae2ca4be353fbc47913e69f0d4e685faba9
AlexDolm <sancho0_0@mail.ru>
2022-04-06 13:02:30 +0300

    task1

--- LogTasks.playground/Contents.swift
+++ LogTasks.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//В коде сторонней библиотеки обнаружился баг. Вместо отсортированного массива строк библиотека возвращает массив случайно упорядоченных строк
+
+var response = "ыв3а\nвы1а\nsd2f"
+var fullNameArr = response.components(separatedBy: "\n")
+var mass = [String]()
+
+
+for i in 1...fullNameArr.count {
+    for j in fullNameArr {
+        if j.contains(String(i)){
+            var perem = [String]()
+            for k in j{
+                if String(k) != String(i){
+                    perem.append(String(k))
+                }
+            }
+            mass.append(perem.joined(separator: ""))
+
+        }
+    }
+
+}
+
+print(mass)
+
